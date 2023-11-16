91fcae2c3f0be3117b6846a28cfaedb80cf16355
RodrigoSante <132940978+RodrigoSante@users.noreply.github.com>
2023-11-16 16:15:59 -0300

    Added project section

--- src/App.js
+++ src/App.js
@@ -33,13 +33,41 @@ function App() {
             <p>Elegancia</p>
             <p>Uno de los principales pilares que nos caracreriza es la elegancia y sofisticacion de nuestros diseños, buscamos explotar al maximo la creatividad de nuestros diseños, pero entrando en la mente de nuestros clientes y encontrando los mejores diseños que se adecuan a sus necesidad, esto es algo que nos caracteriza y nos ha llevado a crear diseños unicos, que llevan nuestra escencia en cada diseño pero expresado a traves de los gustos y necesidades de nuestros clientes.</p>
           </div>
-          <div className='Calidad'>
+          <div className='about-us-container-4'>
             <p>Calidad</p>
             <p>Trabajamos los materiales mas recomendados dentro del mercado de la construccion, y los combinamos para crear espacios armonicos para la comodida de nuestros clientes, sin dejar ningun detalle al azar.</p>
           </div>
-          <div className='Economia'>
+          <div className='about-us-container-5'>
             <p>Economia</p>
             <p>La eficiencia es el ultimo toque dentro de nustros diseños, aprovechamos al maximo cada uno de los materiales, cuidamos el medio ambiente y tambien los recursos de nuestros clientes a la hora de tomar decisiones de diseño.</p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className='Projects'>
+          <div className='projects-container-1'>
+            <p>Te presentamos algunos de nuestros mejores proyectos.</p>
+            <button>Contactanos</button>
+          </div>
+          <div className='projects-container-2'>
+            <p>Image 2</p>
+          </div>
+          <div className='projects-container-3'>
+            <p>Cada uno de nuestros diseños es unico porque se surge de la necesidad de cada cliente.</p>
+            <p>Algúnos números que nos respaldan.</p>
+            <div className='info-projects-container'>
+              <p>Casas Unifamiliares con destino residencial +200</p>
+              <p>Locales comerciales +25</p>
+              <p>Edificios Residenciales +5</p>
+              <p>Efificios Comerciales +3</p>
+            </div>
+          </div>
+          <div className='projects-container-4'>
+            <p>Proyectos destacados.</p>
+            <p>Image 1</p>
+            <p>Image 2</p>
+            <p>Image 3</p>
+            <p>Image 4</p>
           </div>
         </div>
       </section>
